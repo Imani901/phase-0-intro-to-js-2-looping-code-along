e7dbb1932ee5da96c894d4139354749ab62db059
faith <faith.henu@student.moringaschool.com>
2025-03-19 09:42:13 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+// Export the function if you're using modules
+module.exports = { writeCards };
+
+
+function countDown(number) {
+  while (number >= 0) {
+      console.log(number);
+      number--;
+  }
+}
+
+// Export the function if you're using modules
+module.exports = { countDown };
